be33efa67db69f5ce4e46e728fb693e74ec8334f
Patobeur41 <48214878+patobeur@users.noreply.github.com>
2019-11-25 07:50:46 +0100

    Update ikar01.php
    "stripos($_SERVER['PHP_SELF']" added to avoid direct kick

--- ikar/ink/ikar01.php
+++ ikar/ink/ikar01.php
@@ -1,6 +1,6 @@
 <?php
-If ($_SERVER['PHP_SELF']=="ikar01.php"){header("Location: lol.php");}
-if(isset($_POST)){
+if (stripos($_SERVER['PHP_SELF'], 'ink')){header("Location: ../index.html");}
+if(isset($_POST) && isset($_POST['identifiant']) && isset($_POST['motdepasse'])){
     $query = json_decode($_POST["data"]);
     $query->good = md5("HotPass");
     $query->success = false;
